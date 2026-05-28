7c0af26dbda662199cf53ad45650ca436a0efe7d
Tanguturi <brunda.tanguturi@unisys.com>
2026-05-28 20:23:40 +0530

    Clean workflow update1

--- calstate-template2/backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "Grouper"
-#     storage_account_name = "groupertfstate"
-#     container_name       = "terraform"
-#     key                  = "dcs-apps-alb.tfstate"
-#   }
-# }
